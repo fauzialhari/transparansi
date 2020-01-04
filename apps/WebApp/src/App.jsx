612fe13d3fa1f81feca6c29284c0098dba9f91df
Fauzi Alhari <32444439+fauzialhari@users.noreply.github.com>
2020-01-04 23:52:38 +0700

    Import Dasboard on web-app

--- apps/WebApp/src/App.jsx
+++ apps/WebApp/src/App.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
-import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import { ApolloProvider /*useQuery*/ } from '@apollo/react-hooks';
 import client from '@transparansi/store';
-import { Landing } from '@transparansi/web-landing/dist/pages';
-import gql from 'graphql-tag';
-import logo from './logo.svg';
+import Dashboard from '@transparansi/web-dashboard';
+// import gql from 'graphql-tag';
 import './App.css';
 
-const Test = () => {
-  const { data } = useQuery(gql`
-    query GetAllUser {
-      allUsers @client
-    }
-  `);
+// const Test = () => {
+//   const { data } = useQuery(gql`
+//     query GetAllUser {
+//       allUsers @client
+//     }
+//   `);
 
-  return data ? <>ada</> : <>tena</>;
-};
+//   return data ? <>ada</> : <>tena</>;
+// };
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Landing />
-      <div className="App">
-        <header className="App-header">
-          <Test />
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Dashboard />
     </ApolloProvider>
   );
 }
